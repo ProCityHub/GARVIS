6060e0453f6b7f4414e9e2effaca986d98570d9c
Adrien D. Thomas <207450937+ProCityHub@users.noreply.github.com>
2026-07-20 09:21:03 -0600

    Add Lattice psychology equilibrium kernel

---
+++ src/garvis/psychology/__init__.py
@@ -0,0 +1,23 @@
+"""GARVIS Psychology Kernel."""
+
+from .equilibrium import (
+    FULL_ACTIVATION,
+    SIX_WALL_COHERENCE,
+    UNIFIED_SCALE,
+    EquilibriumResult,
+    evaluate_equilibrium,
+    geometric_equilibrium,
+    psychological_coherence,
+    unified_center,
+)
+
+__all__ = [
+    "FULL_ACTIVATION",
+    "SIX_WALL_COHERENCE",
+    "UNIFIED_SCALE",
+    "EquilibriumResult",
+    "evaluate_equilibrium",
+    "geometric_equilibrium",
+    "psychological_coherence",
+    "unified_center",
+]
